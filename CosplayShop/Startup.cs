3960e7c91164b769b51b285ce4260c995ccb8029
mynameisfury <jonathanweatherall0@gmail.com>
2019-05-03 18:07:06 -0500

    init

---
+++ CosplayShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CosplayShop.Startup))]
+namespace CosplayShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
