e3820e8c18b3667f95b98f5891ccadc11da21d70
mynameisfury <jonathanweatherall0@gmail.com>
2019-05-10 15:23:57 -0500

    added a bunch of models'

--- CosplayShop/Models/ZipCode.cs
+++ CosplayShop/Models/ZipCode.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace CosplayShop.Models
 {
-    public class Prop
+    public class ZipCode
     {
-        [Key]
-        public int ID { get; set; }
-
     }
 }
